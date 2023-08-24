b05473f44a793867467dbeb4f29c5c2cde98ec1d
Rashini Gangadarie <rashini@encyteus.com>
2023-08-24 14:35:51 +0530

    Organizations - action button

--- resources/js/components/Superuser/Organizations/Organizations.jsx
+++ resources/js/components/Superuser/Organizations/Organizations.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Grid, Typography } from "@mui/material";
+import { IconButton, Menu, MenuItem, Select, FormControl, InputLabel, ListItemIcon, TextField } from "@mui/material";
 import MainLayout from "../BaseLayout";
 import axios from 'axios';
 import swal from 'sweetalert';
 import { DataGrid, GridColDef, GridValueGetterParams, GridToolbar } from '@mui/x-data-grid';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function Organizations() {
 
@@ -15,12 +18,68 @@ function Organizations() {
         { field: 'email', headerName: 'Email', flex: 1 },
         { field: 'phone', headerName: 'Phone', flex: 1 },
         { field: 'joined', headerName: 'Joined', flex: 1 },
+        {
+            field: 'actions', headerName: '', flex: 1,
+            renderCell: (params) => {
+                return (
+                    <div style={{width: '100%', display: 'flex', justifyContent: 'center'}}>
+                        <div className="action-button-div">
+                            <IconButton
+                                aria-controls="select-menu"
+                                aria-haspopup="true"
+                                onClick={handleOpenMenu}
+                                color="primary"
+                            >
+                                <MoreVertIcon sx={{ color: '#85a129' }} />
+                            </IconButton>
+                            <Menu
+                                id="select-menu"
+                                anchorEl={anchorEl}
+                                keepMounted
+                                open={Boolean(anchorEl)}
+                                onClose={handleCloseMenu}
+                            >
+                                <MenuItem onClick={() => handleOptionSelect('Option 1')}>
+                                    <ListItemIcon>
+                                        <EditIcon />
+                                    </ListItemIcon>
+                                    Update
+                                </MenuItem>
+                                <MenuItem onClick={() => handleOptionSelect('Option 2')}>
+                                    <ListItemIcon>
+                                        <DeleteIcon />
+                                    </ListItemIcon>
+                                    Delete
+                                </MenuItem>
+                            </Menu>
+                        </div>
+                    </div>
+                );
+            }
+        },
     ];
 
     const rows = [
-        { id: 1, organization: 'Organization', type: 'Academia', reg: '123456789', size: '11 - 30',  email: 'organization@feo.lk', phone: '0112938475', joined: '2023-02-23 07:33:22'},
-        { id: 2, organization: 'Organization', type: 'Academia', reg: '123456789', size: '11 - 30',  email: 'organization@feo.lk', phone: '0112938475', joined: '2023-02-23 07:33:22'},
+        { id: 1, organization: 'Organization', type: 'Academia', reg: '123456789', size: '11 - 30', email: 'organization@feo.lk', phone: '0112938475', joined: '2023-02-23 07:33:22' },
+        { id: 2, organization: 'Organization', type: 'Academia', reg: '123456789', size: '11 - 30', email: 'organization@feo.lk', phone: '0112938475', joined: '2023-02-23 07:33:22' },
     ];
+
+
+    const [selectedOption, setSelectedOption] = useState('');
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleOpenMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleCloseMenu = () => {
+        setAnchorEl(null);
+    };
+
+    const handleOptionSelect = (option) => {
+        setSelectedOption(option);
+        handleCloseMenu();
+    };
 
     return (
         <MainLayout>
